87dfa4717eb1aa379fc8151b89d1b30907830b50
Brandon Lewis <brandon_lewis@alum.berkeley.edu>
2011-01-22 17:22:46 +0000

    Move MouseCommandDispatcher to separate module

---
+++ dispatcher.py
@@ -0,0 +1,37 @@
+import gtk
+from behavior import MouseInteraction
+
+class MouseCommandDispatcher(MouseInteraction):
+
+    def __init__(self, undo, drag_commands, click_commands=()):
+        self.selected = None
+        self.item = None
+        self.undo = undo
+        self.mode = None
+        self.command = None
+        self.drag_commands = drag_commands
+        self.click_commands = click_commands
+
+    def drag_start(self):
+        self.command = self.find_command(self.drag_commands)
+    
+    def find_command(self, commands):
+        for command in commands:
+            ret = command.create_for_point(self.instance, self.abs)
+            if ret: return ret
+
+    def move(self):
+        if self.command:
+            shift = not self.event.get_state() & gtk.gdk.SHIFT_MASK
+            self.command.update(self.abs, self.rel, shift)
+
+    def drag_end(self):
+        if self.command:
+            self.undo.commit(self.command)
+        self.command = None
+
+    def click(self):
+        cmd = self.find_command(self.click_commands)
+        if cmd:
+            cmd.do()
+            self.undo.commit(cmd)
