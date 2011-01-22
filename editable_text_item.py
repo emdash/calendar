b8bbe28c4addb2de7f07538d17ec177808231f94
Brandon Lewis <brandon_lewis@alum.berkeley.edu>
2011-01-22 14:57:05 +0000

    EditableTextItem: don't do ghetto hacks to handle keyboard input

--- editable_text_item.py
+++ editable_text_item.py
@@ -5,6 +5,90 @@ import settings
 import cairo
 import pango
 import gtk
+from behavior import Behavior
+
+class TextInput(Behavior):
+
+    def __init__(self, text_changed_func):
+        self.buffer = gtk.TextBuffer()
+        self.buffer.connect("changed", self._buffer_changed_cb)
+        self.text_changed_func = text_changed_func
+
+    def _connect(self):
+        self.connect("key-press-event")
+        self.connect("key-release-event")
+        self.connect("focus-in-event")
+        self.connect("focus-out-event")
+
+    def on_focus_in_event(self, item, target, event):
+        pass
+
+    def on_focus_out_event(self, item, target, event):
+        pass
+
+    def cursor_iter(self):
+        return self.buffer.get_iter_at_mark(self.buffer.get_insert())
+
+    def delete(self, control):
+        fr = self.cursor_iter()
+        if control:
+            fr.backward_word_start()
+        else:
+            fr.backward_cursor_position()
+        to = self.cursor_iter()
+        self.buffer.delete(fr, to)
+            
+    def cursor_motion_func(func):
+        def cursor_motion_func(self, control):
+            i = self.cursor_iter()
+            func(i, control)
+            self.buffer.place_cursor(i)
+        return cursor_motion_func
+
+    @cursor_motion_func
+    def left(i, control):
+        if control:
+            i.backward_word_start()
+        else:
+            i.backward_cursor_position()
+
+    @cursor_motion_func
+    def right(i, control):
+        if control:
+            i.forward_word_end()
+        else:
+            i.forward_cursor_position()
+
+    keyfuncs = {
+        gtk.keysyms.BackSpace: delete,
+        gtk.keysyms.Left: left,
+        gtk.keysyms.Right: right,
+    }
+
+    def on_key_press_event(self, item, target, event):
+        self.instance.cursor_showing = True
+
+        if event.keyval in self.keyfuncs:
+            self.keyfuncs[event.keyval](self, event.state & gtk.gdk.CONTROL_MASK)
+            self.text_changed_func()
+            return
+        self.buffer.insert_at_cursor(event.string)
+
+    def on_key_release_event(self, item, target, event):
+        pass
+
+    def get_text(self):
+        return self.buffer.get_text(*self.buffer.get_bounds())
+
+    def set_text(self, text):
+        self.buffer.set_text(text)
+        self.cursor = self.buffer.get_end_iter()
+
+    def get_cursor_pos(self):
+        return self.cursor_iter().get_offset()
+
+    def _buffer_changed_cb(self, buffer):
+        self.text_changed_func()
 
 class EditableTextItem(goocanvas.ItemSimple, goocanvas.Item):
 
@@ -14,58 +98,31 @@ class EditableTextItem(goocanvas.ItemSimple, goocanvas.Item):
     y = gobject.property(type=int, default=0)
     width = gobject.property(type=int, default=0)
     height = gobject.property(type=int, default=0)
-    text = gobject.property(type=str, default="text")
     show_frame = gobject.property(type=bool, default=True)
 
+    def get_text(self):
+        return self.ti.get_text()
+
+    def set_text(self, text):
+        return self.ti.set_text(text)
+
+    text = gobject.property(get_text, set_text, type=str)
+
     def __init__(self, *args, **kwargs):
+        self.ti = TextInput(lambda : self.changed(False))
         goocanvas.ItemSimple.__init__(self, *args, **kwargs)
         self.connect("notify", self.do_notify)
-        self.connect("key-press-event", self._key_press_event)
-        self.connect("key-release-event", self._key_release_event)
-        self.connect("focus-in-event", self._focus_in_event)
-        self.connect("focus-out-event", self._focus_out_event)
         self.cursor_showing = True
         self.focused = False
-        self.set_proxy(gtk.Entry())
         gobject.timeout_add(500, self._blink_cursor)
-
-
-    def _focus_in_event(self, item, target, event):
-        pass
-
-    def _focus_out_event(self, item, target, event):
-        pass
+        self.ti.observe(self)
 
     def _blink_cursor(self):
         self.cursor_showing = not self.cursor_showing
         self.changed(False)
         return True
 
-    def _key_press_event(self, item, target, event):
-        if self.proxy:
-            self.proxy.emit("key-press-event", event)
-        self.cursor_showing = True
-
-    def _key_release_event(self, item, target, event):
-        if self.proxy:
-            self.proxy.emit("key-release-event", event)
-
-    def set_proxy(self, proxy):
-        self.proxy = proxy
-        self.proxy.set_text(self.text)
-        self.proxy.connect("changed",
-                           self._proxy_changed_cb)
-        self.proxy.connect("notify::cursor-position", self._proxy_notify_curspor_position_cb)
-
-    def _proxy_notify_curspor_position_cb(self, proxy, pspec):
-        self.changed(False)
-
-    def _proxy_changed_cb(self, entry):
-        self.text = entry.get_text()
-
     def do_notify(self, unused, pspec):
-        if pspec.name == "text":
-            self.proxy.set_text(self.text)
         self.changed(True)
 
     def do_simple_is_item_at(self, x, y, cr, pointer_event):
@@ -88,7 +145,7 @@ class EditableTextItem(goocanvas.ItemSimple, goocanvas.Item):
     def get_cursor_pos(self, lyt):
         return [pango.units_to_double(x)
                 for x in
-                lyt.get_cursor_pos(self.proxy.props.cursor_position)[0]]
+                lyt.get_cursor_pos(self.ti.get_cursor_pos())[0]]
 
     def draw_cursor(self, cr, lyt):
         cr.save()
@@ -97,13 +154,13 @@ class EditableTextItem(goocanvas.ItemSimple, goocanvas.Item):
         if not self.cursor_showing:
             return
         x, y, width, height = self.get_cursor_pos(lyt)
-        cr.move_to(self.x + x, self.y + y)
-        cr.line_to(self.x + x, self.y + y + height)
+        cr.move_to(self.x + x + 2, self.y + y)
+        cr.line_to(self.x + x + 2, self.y + y + height)
         cr.stroke()
         cr.restore()
 
     def draw_text(self, cr):
-        lyt = left_aligned_text(cr, self.text, self.x, self.y, self.width, self.height,
+        lyt = left_aligned_text(cr, self.ti.get_text(), self.x + 2, self.y, self.width - 4, self.height,
                                 settings.text_color)
         self.draw_cursor(cr, lyt)
 
